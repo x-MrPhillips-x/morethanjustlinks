98154ec638ee953604e080d5da67b032bd6436b0
x-MrPhillips-x <terrell.lp@gmail.com>
2023-04-10 00:41:47 -0500

    debug route issue

--- handler/handler.go
+++ handler/handler.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	maria_db "example.com/morethanjustlinks/db"
-	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
@@ -15,7 +14,7 @@ type HandlerService struct {
 	sugaredLogger *zap.SugaredLogger
 }
 
-var secret = []byte("secret")
+// var secret = []byte("secret")
 var PING_DB_ATTEMPTS = 90
 
 func NewHandlerService(maria_repo maria_db.DbInterface, sugaredLogger *zap.SugaredLogger, attempts int) (*HandlerService, error) {
@@ -38,57 +37,37 @@ func (h *HandlerService) SetupHandlerServiceRoutes() *gin.Engine {
 	router := gin.Default()
 	gin.SetMode(gin.ReleaseMode)
 
-	cookieStore := sessions.NewCookieStore(secret)
+	// cookieStore := sessions.NewCookieStore(secret)
 
-	router.Use(
-		sessions.Sessions("mysession", cookieStore),
-	)
+	// router.Use(
+	// 	sessions.Sessions("mysession", cookieStore),
+	// )
 
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"msg": "pong",
 		})
 	})
-	router.GET("/setup", h.SetupService)
-	router.POST("login", h.Login)
-	router.GET("logout", h.Logout)
-	router.POST("newAccount", h.NewAccount)
-	router.GET("/:name", h.GetProfile)
-	router.GET("/getAllUsers", h.GetAllUsers)
+	// router.GET("/setup", h.SetupService)
+	// router.POST("login", h.Login)
+	// router.GET("logout", h.Logout)
+	// router.POST("newAccount", h.NewAccount)
+	// router.GET("/:name", h.GetProfile)
+	// router.GET("/getAllUsers", h.GetAllUsers)
 
-	// todo profile router use authentication
-	// does pfp need to use sessions also?
-	pfp := router.Group("/:name/profile", h.Authentication)
-	pfp.POST("/edit", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"msg": "place holder for profile updates",
-		})
-	})
-
-	// TODO and ^ behind pfp
-	router.POST("/deleteUser", h.DeleteUser)
-	router.POST("/deleteLink", h.DeleteLink)
-	router.POST("/update", h.UpdateUser)
-
-	// 		username := ctx.Param("name")
-	// 		if v == nil {
-	// 			count = 0
-	// 		} else {
-	// 			count = v.(int)
-	// 			count += 1
-	// 		}
-	// 		session.Set("count", count)
-	// 		session.Save()
-	// 		ctx.HTML(http.StatusOK, "profile.tmpl", gin.H{
-	// 			"count": count,
-	// 			"name":  username,
-	// 		})
+	// // todo profile router use authentication
+	// // does pfp need to use sessions also?
+	// pfp := router.Group("/:name/profile", h.Authentication)
+	// pfp.POST("/edit", func(ctx *gin.Context) {
+	// 	ctx.JSON(http.StatusOK, gin.H{
+	// 		"msg": "place holder for profile updates",
 	// 	})
-	// 	api.GET("/setup", h.SetupService)
-	// 	api.GET("/getAllUsers", h.GetAllUsers)
-	// 	api.POST("/delete", h.DeleteUser)
-	// 	api.POST("/update", h.UpdateUser)
-	// }
+	// })
+
+	// // TODO and ^ behind pfp
+	// router.POST("/deleteUser", h.DeleteUser)
+	// router.POST("/deleteLink", h.DeleteLink)
+	// router.POST("/update", h.UpdateUser)
 
 	return router
 }
