cdc49c649b1e39508508867e05db022b72ddf5c0
x-MrPhillips-x <terrell.lp@gmail.com>
2023-04-11 14:33:10 -0500

    init react docker

---
+++ frontend/App.js
@@ -0,0 +1,19 @@
+
+function App() {
+  return (
+    <div>
+        <div>Your logo here</div>
+        
+        <form action="/login" method="post" onsubmit="submitForm(event)">
+        <div>Mobile/Username/Email <input type="text" name="name" id="name" required/></div>
+        <div>Password <input type="password" name="psword" id="psword" required/></div>
+        <div><input type="submit" value="Login"/></div>
+        <div><a href="/newAccountForm">Create a new account</a> | <a href="#">Forgot password?</a> </div>
+        </form>
+
+        <footer>Copyright &copy; 2023 yourUserName All Rights Reserved</footer>
+    </div>
+  );
+}
+
+export default App;
